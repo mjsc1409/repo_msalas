40f2d7f508d155de3ceb01779c5b0fa8c782a1c2
mjsc1409 <80779469+mjsc1409@users.noreply.github.com>
2021-09-03 12:38:31 -0500

    Create docker_oneclick_install.sh

---
+++ docker_oneclick_install.sh
@@ -0,0 +1,17 @@
+sudo apt-get update
+sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --batch --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli
+sed -i '13s/.*/ExecStart=\/usr\/bin\/dockerd -H=fd:\/\/ -H=tcp:\/\/0.0.0.0:2375/' /lib/systemd/system/docker.service
+systemctl daemon-reload
+sudo service docker restart
+curl http://localhost:2375/images/json
